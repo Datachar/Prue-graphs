5329f61971061106688c38c03943c087c438e19a
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-03-26 19:20:41 +0200

    SHIT CODE from me please don't read !!!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from textwrap import wrap
+import matplotlib.gridspec as gridspec
 
 
 def main():
@@ -42,13 +43,27 @@ def main():
         '/home/misha/Downloads/Data Sets 24-03/Stress Management.xlsx',
         '/home/misha/Downloads/Data Sets 24-03/Web Development.xlsx'
     ]
-    header_datas = create_header(file_paths)
+    header_data = create_header(file_paths)
     dfs = [pd.read_excel(io=file_path, skiprows=6) for file_path in file_paths]
-    fb_datas = [fb_page_for_marketing(df) for df in dfs]
-    # draw_pie(fb_data, 'Facebook Page for Marketing', header_data)
-    # [print(fb) for fb in fb_datas]
-    # [print(fb) for fb in header_datas]
-    new_draw(fb_datas, header_datas)
+    all_data = [fb_page_for_marketing(df) for df in dfs]
+    new_draw(dfs, header_data, all_data)
+
+
+def all_file_category(header_data):
+    all_data = []
+    for i in header_data:
+        if str(i['Category']) not in all_data:
+            all_data.append(i['Category'])
+    return all_data
+
+
+def create_data_for_category(df, header_data, category):
+    data = []
+    for i, df_ in enumerate(df):
+        if header_data[i]['Category'] == category:
+            d = fb_page_for_marketing(df_)
+            data.append(d)
+    return data
 
 
 def fb_page_for_marketing(df):
@@ -84,51 +99,70 @@ def create_header(file_path_):
     return data_
 
 
-def draw_pie(data, title, header_data):
-    labels = list(data.keys())
-    sizes = list(data.values())
-    colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
-    plt.pie(sizes, labels=labels, colors=colors,
-            autopct='%1.1f%%', startangle=90)
-    plt.title(title)
-    plt.axis('equal')
-    fig = plt.gcf()
-    fig.set_size_inches(18.5, 10.5)
-    plt.savefig(header_data['Subcategory'] + '.png', dpi=100)
-
-
-def new_draw(datas, header_datas):
-    ylabel = list(datas[0].keys())
-    y = np.array([list(val.values()) for val in datas])
-    plt.subplot(3, 1, 1)
+def new_draw(df,header_datas, all_data):
+    all_category = all_file_category(header_datas)
+    count = len(all_category)
+    colors = ['#EE5363', '#57CCC6', '#F2B354']
+    width = 1
+    y_step = np.arange(0, 100, 10)
+    gs = gridspec.GridSpec(8, len(all_data)+2*len(all_category)+5)
+    print(len(all_data)+3*len(all_category))
+    size_2 = 0
+    for k in range(1, count+1):
+        datas = create_data_for_category(df, header_datas, all_category[k-1])
+        ylabel = list(datas[0].keys())
+        y = np.array([list(val.values()) for val in datas])
+        plt.subplots_adjust(hspace=.001)
+        x_step = np.arange(len(datas))
+        ylabel = ['\n'.join(wrap(l, 20)) for l in ylabel]
+        for i in range(3):
+            data = [y[j][i] for j in range(len(y))]
+            free_data = [(100 - y[j][i]) for j in range(len(y))]
+            size_ = len(data)
+            print(i, size_2, size_2+size_)
+            plt.subplots_adjust(hspace=.001, wspace=0.45)
+            plt.subplot(gs[i, size_2: size_2+size_])
+            plt.ylabel(ylabel[i], rotation='horizontal', horizontalalignment='right')
+            plt.bar(x_step, data, width, color=colors[i], label='787')
+            text = ["{:10.1f}%".format(d) for d in data]
+            for j in range(len(y)):
+                plt.text(x_step[j] + width/4, data[j] - 5, text[j], horizontalalignment='center',
+                         verticalalignment='center', color='black', weight='bold', size=11)
+            plt.bar(x_step, free_data, width, color='w', bottom=data)
+            plt.xticks(x_step,)
+            plt.yticks(y_step, '')
+        name_subcategory = [data['Subcategory'] for data in header_datas if str(data['Category']) == str(all_category[k-1])]
+        plt.xticks(x_step + width / 2, name_subcategory, rotation=90, size=7)
+        plt.xlabel(all_category[k-1], weight='bold', size=16)
+        size_2 += size_ + 3
+    ylabel = list(all_data[0].keys())
+    y = np.array([list(val.values()) for val in all_data])
+    plt.subplot(gs[5,:])
     plt.subplots_adjust(hspace=.001)
     plt.title('Facebook Page for Marketing', size=25, weight='heavy')
-    x_step = np.arange(len(datas))
-    y_step = np.arange(0, 100, 10)
-    width = 0.85
-    colors = ['r', 'y', 'g']
+    x_step = np.arange(len(all_data))
     ylabel = ['\n'.join(wrap(l, 20)) for l in ylabel]
     for i in range(3):
         data = [y[j][i] for j in range(len(y))]
         free_data = [(100 - y[j][i]) for j in range(len(y))]
-        plt.subplot(3, 1, i + 1)
+        plt.subplot(gs[i+5,4:40])
         plt.subplots_adjust(hspace=.001)
         plt.ylabel(ylabel[i], rotation='horizontal', horizontalalignment='right')
         plt.bar(x_step, data, width, color=colors[i], label='787')
         text = ["{:10.1f}%".format(d) for d in data]
         for i in range(len(y)):
-            plt.text(x_step[i] + width / 4, data[i] - 5, text[i], horizontalalignment='center',
-                     verticalalignment='center', color='black', weight='bold', size=6)
+            plt.text(x_step[i] + width / 2, data[i] - 5, text[i], horizontalalignment='center',
+                     verticalalignment='center', color='black', weight='bold', size=12)
         plt.bar(x_step, free_data, width, color='w', bottom=data)
         plt.xticks(x_step, '')
         plt.yticks(y_step, '')
     name_subcategory = [data['Subcategory'] for data in header_datas]
-    plt.xticks(x_step + width / 2, name_subcategory, rotation=90, size=7)
-    plt.xlabel(header_datas[0]['Category'])
+    plt.xticks(x_step + width / 4, name_subcategory, rotation=90, size=7)
+    plt.xlabel('All category',weight='bold', size=16)
     fig = plt.gcf()
-    fig.set_size_inches(18.5, 15.5)
-    # plt.savefig(header_datas[0]['Category'] + '.png', dpi=100)
-    plt.show()
+    fig.set_size_inches(40, 25)
+    plt.savefig('Graphs.png', dpi=150)
+    #plt.show()
 
 
 if __name__ == '__main__':
